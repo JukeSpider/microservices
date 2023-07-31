37664226269879faa0296c2a6399a0a35f6c2f2e
Aleksandr Paukov <a.paukov@strongte.am>
2023-07-31 13:23:06 +0600

    JMP-1004 Add annotation @ConfigurationPropertiesScan

--- auth-service/src/main/kotlin/com/juke/auth/AuthServiceApplication.kt
+++ auth-service/src/main/kotlin/com/juke/auth/AuthServiceApplication.kt
@@ -1,9 +1,11 @@
 package com.juke.auth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
+@ConfigurationPropertiesScan
 @EnableR2dbcRepositories
 @SpringBootApplication
 class AuthServiceApplication
