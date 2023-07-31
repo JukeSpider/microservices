80598d57c061a8bfd2239ba9c37f33cd2772f0a1
Aleksandr Paukov <a.paukov@strongte.am>
2023-07-31 14:05:54 +0600

    JMP-1008 Add Spring Security configuration

---
+++ auth-service/src/main/kotlin/com/juke/auth/core/domain/failure/AccessDeniedFailure.kt
@@ -0,0 +1,7 @@
+package com.juke.auth.core.domain.failure
+
+data class AccessDeniedFailure(
+    override val code: Int = 403,
+    override val status: String = "access_denied",
+    override val message: String = "Access Denied!"
+) : Failure
